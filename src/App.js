809d52be6918d8f4814a9a07b168e4fad31ac429
adriangarcia <adrian1994215@gmail.com>
2019-08-07 11:25:19 -0500

    actualizacion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
+import Header from './Global/Header/Header';
+import Content from './Global/Content/Content';
+import Footer from './Global/Footer/Footer';
+import items from './data/menu';
+import Login from "./components/Login/Login";
+import Dashboard from './components/Dashboard/Dashboard';
+import Navbar from './components/Dashboard/Navbar';
+import fire from './config/fire';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state={
+      user: {}
+    };
+  }
+componentDidMount(){
+    this.authListener();
+  }
+  authListener(){
+    fire.auth().onAuthStateChanged((user)=>{
+      if(user){
+        this.setState({user});
+      }else{
+        this.setState({user: null});
+      }
+    });
+  }
+
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  };
+
+  render(){
+    const {children} = this.props;
+    return (
+        <div className="App">
+        {this.state.user ? (Navbar):(<Header items={items}/>)}
+        <Content body={this.state.user ? (<Dashboard/>):(children)}/>
+        <Footer/>
+        </div>
+      );
+  }
 }
+
 
 export default App;
